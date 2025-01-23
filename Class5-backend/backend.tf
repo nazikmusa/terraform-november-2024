a5a831ab0f2e56f6dbe2f5942f528465aff36688
nazikmusa <musaevan331@gmail.com>
2025-01-23 03:06:01 +0000

    taint command

---
+++ Class5-backend/backend.tf
@@ -0,0 +1,9 @@
+# terraform {
+#   backend "s3" {
+#     bucket = "kaizen-nazgylm"
+#     key    = "terraform.tfstate"
+#     region = "us-east-1"
+#     dynamodb_table = "lock-state"
+#   }
+
+# }
--- Class5-backend/main.tf
+++ Class5-backend/main.tf
@@ -10,4 +10,11 @@ variable region {
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
   public_key = file("~/.ssh/id_rsa.pub")
+
+tags = {
+    Team = "DevOps"
+    Environment = "Dev"
+    Owner = "Nazgul"
 }
+}
+
