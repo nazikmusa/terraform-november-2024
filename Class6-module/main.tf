37ecd12283a35083a2feb89bd67894813f6ba5a9
nazikmusa <musaevan331@gmail.com>
2025-01-25 02:52:46 +0000

    test folder

---
+++ Class6-module/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.region
+}
+
+resource aws_vpc main {
+  cidr_block =  var.vpc_cidr
+}
+
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.subnet_cidr
+}
