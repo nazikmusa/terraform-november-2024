c03336518218ba3a84a184fe3988575f72e85b76
nazikmusa <musaevan331@gmail.com>
2025-01-25 01:09:45 +0000

    provisioner exec

---
+++ Class6/main.tf
@@ -0,0 +1,35 @@
+provider aws {
+    region = "us-east-2"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0eb070c40e6a142a3"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.deployer.key_name
+
+   connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    host     = self.public_ip
+    private_key = file ("~/.ssh/id_rsa")
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+        "sudo yum install httpd -y"
+    ]
+  }
+}
+
+# resource "null_resource" "cluster" {
+#       provisioner "local-exec" {
+#    command = "mkdir kaizen && touch hello"
+#  }
+  
+# }
+
