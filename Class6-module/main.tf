8fc0d921a921474cd3282a79b21504e78d14f11a
nazikmusa <musaevan331@gmail.com>
2025-02-01 22:56:58 +0000

    module used from io

--- Class6-module/main.tf
+++ Class6-module/main.tf
@@ -1,12 +1,6 @@
-provider "aws" {
-  region = var.region
-}
-
-resource aws_vpc main {
-  cidr_block =  var.vpc_cidr
-}
-
-resource "aws_subnet" "main" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.subnet_cidr
+module "november-2024" {
+  source  = "nazikmusa/november-2024/module"
+  version = "1.0.0"
+  region = "us-east-1"
+  vpc_cidr = "10.0.0.0/16"
 }
