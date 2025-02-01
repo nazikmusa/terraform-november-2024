17af15c41d04423db062eabfbec60204bd566260
nazikmusa <musaevan331@gmail.com>
2025-02-01 18:18:18 +0000

    backend file created

--- Class5-backend/backend.tf
+++ Class5-backend/backend.tf
@@ -1,9 +1,10 @@
 # terraform {
 #   backend "s3" {
-#     bucket = "kaizen-nazgylm"
+#     bucket = "nazgul-musaeva"
 #     key    = "terraform.tfstate"
 #     region = "us-east-1"
 #     dynamodb_table = "lock-state"
 #   }
 
 # }
+
--- Class5-backend/main.tf
+++ Class5-backend/main.tf
@@ -1,20 +1,17 @@
 provider aws {
-    region = var.region
-}
-
-variable region {
-  default = ""
-  type = string
+    region = "us-east-2"
 }
 
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
   public_key = file("~/.ssh/id_rsa.pub")
 
-tags = {
+  tags = {
     Team = "DevOps"
     Environment = "Dev"
     Owner = "Nazgul"
-}
+    Team2 = "QA"
+  }
+
 }
 
