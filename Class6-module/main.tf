56d9da0fd494167785b83eea8262e0b2f6cd7360
nazikmusa <musaevan331@gmail.com>
2025-02-02 00:48:55 +0000

    updated version

--- Class6-module/main.tf
+++ Class6-module/main.tf
@@ -1,6 +1,7 @@
 module "november-2024" {
   source  = "nazikmusa/november-2024/module"
-  version = "1.0.0"
+  version = "2.0.0"
   region = "us-east-1"
   vpc_cidr = "10.0.0.0/16"
+  subnet_cidr = "10.0.1.0/24"
 }
